c8e259c5d444090d674496592d86271e8cd9cadd
Yang Gyeongmo <yangkyeongmo@snu.ac.kr>
2018-11-27 22:55:41 +0900

    applied 'sliding'

--- filemoverandom.py
+++ filemoverandom.py
@@ -1,12 +1,30 @@
 import os
 import argparse
+import random
+
+def main(arg):
+    path = arg.path
+    if not os.path.exists(path):
+        print "NO PATH ERROR: " + path
+        return
+    dest = arg.dest
+    if not os.path.exists(dest):
+        os.mkdir(dest)
+    amount = arg.amount
+    print 'Start moving ' + path + ' to ' + dest + ' for ' + amount.__str__() + ' items'
+    
+    movedamount = 0
+    while movedamount < amount:
+        rand = random.random()
+        filelist = os.listdir(path)
+        if rand > 0.5:
+            randidx = random.randrange(0, len(filelist))
+            os.rename(os.path.join(path,filelist[randidx]), os.path.join(dest,filelist[randidx]))
+            movedamount = movedamount + 1
+    print 'Jobs done for ' + movedamount.__str__() + ' items'
 
 
-def main(path, dest):
-
-
-
-if __name__='__main__':
+if __name__=='__main__':
 	parser = argparse.ArgumentParser()
 	parser.add_argument(
 		'-p','--path',
@@ -16,5 +34,10 @@ if __name__='__main__':
 		'-d', '--dest',
 		action='store'
 	)
+        parser.add_argument(
+                '-am', '--amount',
+                action = 'store',
+                type=int
+        )
 	arg = parser.parse_args()
-	main(arg.path, arg.dest)
+	main(arg)
