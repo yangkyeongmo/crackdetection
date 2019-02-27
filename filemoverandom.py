87dffcd10cb2a31828046b5cd4329918dbfa5cbb
Yang Gyeongmo <yangkyeongmo@snu.ac.kr>
2019-02-27 12:40:26 +0900

    Refactoring and cleared table

--- filemoverandom.py
+++ filemoverandom.py
@@ -4,68 +4,33 @@ import random
 from shutil import copy
 
 
-MAX_BUFFER_LENGTH = 100
-MAX_TRY_LIMIT = 10000
+MAX_TRY_LIMIT = 100
 
 
-def buffer_in(buffer, src, dest):
-    buffer.append([src, dest])
-    return buffer
-
-
-def buffer_flush(buffer, do_move):
-    processed_amount = 0
-    for x in buffer:
-        if check_file_existence(x[0], x[1]):
-            if do_move:
-                os.rename(x[0], x[1])
-            else:
-                copy(x[0], x[1])
-            processed_amount += 1
-    buffer = []
-    return buffer, processed_amount
-
-
-def check_file_existence(src, dest):
-    if os.path.basename(src) == os.path.basename(dest):
+def check_file_existence_bydir(src, destlist):
+    if any(os.path.basename(src) == os.path.basename(f)
+           for f in destlist):
         return True
     return False
 
-def check_file_existence_bydir(src, dest_dir):
-    if all(os.path.basename(src) != os.path.basename(f) for f in os.listdir(dest_dir)):
-        return False
-    return True
 
-
-def moverandom(arg):
-    path = arg.path
-    dest = arg.dest
-    amount = arg.amount
-    path_buffer = []
-    print('Start moving ' + path + ' to ' + dest + ' for ' + amount.__str__() + ' items')
-    movedamount = 0
-    while movedamount < amount:
-        rand = random.random()
-        filelist = os.listdir(path)
-        if rand > 0.5:
-            randidx = random.randrange(0, len(filelist))
-            target_path = os.path.join(path, filelist[randidx])
-            dest_path = os.path.join(dest, filelist[randidx])
-            os.rename(target_path, dest_path)
-            movedamount += 1
-    buffer_flush(buffer=path_buffer, do_move=arg.move)
-    return movedamount
+def get_copy_path(path, filelist, destlist):
+    for i in range(MAX_TRY_LIMIT):
+        copy_path = os.path.join(path,
+                                 filelist[random.randrange(0, len(filelist))])
+        if not check_file_existence_bydir(src=copy_path, destlist=destlist):
+            break
+    return copy_path
 
 
 def check_all_copied(filelist, destlist):
     all_copied = False
     for f_path in filelist:
         for f_dest in destlist:
-            if os.path.basename(f_dest) != os.path.basename(f_path):
-                all_copied = False
-            else:
+            if os.path.basename(f_dest) == os.path.basename(f_path):
                 all_copied = True
                 break
+            all_copied = False
     return all_copied
 
 
@@ -75,59 +40,62 @@ def check_copy_end(filelist, destlist, amount, end_at_amount):
             print("Everything is copied")
             return True
     if len(destlist) == amount and end_at_amount:
-        print("Ending at amount " + amount.__str__())
+        print("Ending at amount", str(amount))
         return True
     return False
 
 
 def copyrandom(arg):
+    print('Start coping', arg.path, 'to', arg.dest,
+          'for', str(arg.amount), 'items')
+    count = 0
+    while count < arg.amount:
+        filelist = os.listdir(arg.path)
+        destlist = os.listdir(arg.dest)
+        # check if everything is copied
+        if check_copy_end(filelist, destlist, arg.amount, arg.end_at_amount):
+            break
+        copy(get_copy_path(arg.path, filelist, destlist), arg.dest)
+        count += 1
+    return count
+
+
+def moverandom(arg):
     path = arg.path
     dest = arg.dest
-    amount = arg.amount
-    end_at_amount = arg.end_at_amount
-    print('Start coping ' + path + ' to ' + dest + ' for ' + amount.__str__() + ' items')
-    copiedamount = 0
-    existing_lists = os.listdir(dest)
-    while copiedamount < amount:
+    print('Start moving', path, 'to', dest, 'for', str(arg.amount), 'items')
+    count = 0
+    while count < arg.amount:
         filelist = os.listdir(path)
-        destlist = os.listdir(dest)
-        # check if everything is copied
-        if check_copy_end(filelist, destlist, amount, end_at_amount):
-            break
-        copy_path = ""
-        # loop for MAX_TRY_LIMIT to find file not in path
-        for i in range(0, MAX_TRY_LIMIT):
-            randidx = random.randrange(0, len(filelist))
-            copy_path = os.path.join(path, filelist[randidx])
-            if not check_file_existence_bydir(src=copy_path, dest_dir=dest):
-                existing_lists.append(copy_path)
-                break
-        copy(copy_path, dest)
-        copiedamount += 1
-    return copiedamount
+        rand_filename = filelist[random.randrange(0, len(filelist))]
+        os.rename(os.path.join(path, rand_filename),
+                  os.path.join(dest, rand_filename))
+        count += 1
+    return count
+
+
+def _check_path_and_dest(path, dest):
+    if not os.path.exists(path):
+        print("NO PATH ERROR:", path)
+        return
+    if not os.path.exists(dest):
+        os.mkdir(dest)
 
 
 def main(arg):
-    path = arg.path
-    if not os.path.exists(path):
-        print("NO PATH ERROR: " + path)
-        return
-    dest = arg.dest
-    if not os.path.exists(dest):
-        os.mkdir(dest)
-    amount = arg.amount
+    _check_path_and_dest(arg.path, arg.dest)
     processed_amount = 0
     if arg.move:
         processed_amount = moverandom(arg)
     else:
         processed_amount = copyrandom(arg)
-    print('Jobs done for ' + processed_amount.__str__() + ' items')
+    print('Jobs done for', str(processed_amount), 'items')
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        '-p','--path',
+        '-p', '--path',
         action='store'
     )
     parser.add_argument(
